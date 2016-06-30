e160be8bc0caaf99808c8645f897e3ae8ba0fec4
Grover <grover.org.rf@gmail.com>
2016-06-30 18:25:36 -0300

    Add TRAVEL problem with input, output and solution files

---
+++ problem-set/solutions/TRAVEL.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		double vt,vo,d;
+		cin >> vt >> vo >> d;
+		if(vt <= vo) printf("-1\n");
+		else printf("%.4lf\n",d/(vt - vo));
+	}
+	return 0;
+}
+
